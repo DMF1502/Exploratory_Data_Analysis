cdb330f9ab88b3862583815b4aceef11af7bfc54
DMF1502 <68090600+DMF1502@users.noreply.github.com>
2021-06-23 11:56:51 -0500

    Create Plot1.r

---
+++ Plot1.r
@@ -0,0 +1,16 @@
+# set working directory to where you downloaded the file
+setwd(dir = "C:/Users/Daniela/Documents/R Coursera")
+# read file
+AllData <- read.table('C:/Users/Daniela/Documents/R Coursera/household_power_consumption.txt', sep = ';', nrows = 2075259, header = TRUE, quote = " ", stringsAsFactors = FALSE, strip.white = TRUE, na.strings = "?")
+# check to see if it was downloaded properly
+head(AllData)
+# subset data from 1/2/2007 and 2/2/2007
+Feb2007 <- subset(AllData, (AllData$Date == "1/2/2007" | AllData$Date == "2/2/2007"))
+# change characters to dates
+Feb2007$Date <- as.Date(Feb2007$Date, format = "%d/%m/%Y")
+# create histogram
+hist(Feb2007$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+# save to PNG file
+png("Plot1.png", width = 480, height = 480)
+# set device off
+dev.off()
