0db159e9791e3b9d68a1a8433e01a05379ca1b36
DMF1502 <68090600+DMF1502@users.noreply.github.com>
2021-06-23 12:11:09 -0500

    Update Plot1.r

--- Plot1.r
+++ Plot1.r
@@ -8,9 +8,9 @@ head(AllData)
 Feb2007 <- subset(AllData, (AllData$Date == "1/2/2007" | AllData$Date == "2/2/2007"))
 # change characters to dates
 Feb2007$Date <- as.Date(Feb2007$Date, format = "%d/%m/%Y")
+# open PNG file
+png("Plot1.png", width = 480, height = 480)
 # create histogram
 hist(Feb2007$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
-# save to PNG file
-png("Plot1.png", width = 480, height = 480)
 # set device off
 dev.off()
